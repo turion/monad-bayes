37a292a4b03d5ff5f42bfe681a0468e29ad59c76
Manuel Bärenz <m.baerenz@sonnen.de>
2023-02-07 15:59:54 +0100

    WIP

--- src/Control/Monad/Bayes/DelayedSampling.hs
+++ src/Control/Monad/Bayes/DelayedSampling.hs
@@ -4,7 +4,8 @@
 module Control.Monad.Bayes.DelayedSampling where
 
 -- base
-import Data.Maybe (listToMaybe)
+import Data.Maybe (listToMaybe, fromMaybe)
+import Data.Typeable (cast, Typeable)
 
 -- transformers
 import Control.Monad.Trans.Class
@@ -13,60 +14,96 @@ import Control.Monad.Trans.State.Strict
 
 -- monad-bayes
 import Control.Monad.Bayes.Class hiding (Distribution)
+import Statistics.Function (square)
+import Control.Monad.IO.Class (MonadIO (liftIO))
 
-newtype Variable   = Variable Int
-  deriving Show
+newtype Variable a = Variable Int
+  deriving (Show, Eq)
 
 -- FIXME do I need const here and realized later? Or can I get away with only variables?
 -- I probably need const here still in order to deal with a num instance for value
+-- Would be nice to have Functor & Applicative, but that is hard because we also need typeable
 data Value a where
-  Var :: Variable -> Value Double
+  Var :: Variable a -> Value a
   Const :: a -> Value a
-  VarPlus :: Variable -> Double -> Value Double
-  Fmap :: (a -> b) -> Value a -> Value b
-  Ap :: Value (a -> b) -> Value a -> Value b
+  VarPlus :: Num a => Variable a -> a -> Value a
+  Plus :: Num a => Value a -> Value a -> Value a
+  Multiply :: Num a => Value a -> Value a -> Value a
+  Abs :: Num a => Value a -> Value a
+  Signum :: Num a => Value a -> Value a
+  Negate :: Num a => Value a -> Value a
 
-instance Functor Value where
-  fmap = Fmap
+deriving instance Show a => Show (Value a)
+deriving instance Eq a => Eq (Value a)
 
-instance Applicative Value where
-  pure = Const
-  (<*>) = Ap
-
-instance Num a => Num (Value a) where
+instance (Num a, Typeable a) => Num (Value a) where
   Var var + Const a = VarPlus var a
   Const a + Var var = VarPlus var a
-  val1 + val2 = (+) <$> val1 <*> val2
-  val1 * val2 = (*) <$> val1 <*> val2
-  abs = fmap abs
-  signum = fmap signum
-  fromInteger = pure . fromInteger
-  negate = fmap negate
--- FIXME now I don't have show anymore because of the arbitrary function. any chance to get it back?
+  val1 + val2 = Plus val1 val2
+  -- val1 + val2 = (+) <$> val1 <*> val2
+  (*) = Multiply
+  abs = Abs
+  signum = Signum
+  fromInteger = Const . fromInteger
+  negate = Negate
+-- FIXME When adding fmap: now I don't have show anymore because of the arbitrary function. any chance to get it back?
 -- By showing a blackbox for a function? By overloading lambdas?
 
 -- FIXME: Use syb (or recursion-schemes?) to push variable indices by an Int
 
-data Distribution
-  = Normal (Value Double) (Value Double)
-  | Beta (Value Double) (Value Double)
+-- FIXME Maybe I should put the functor bla etc. here?
+-- Or parametrize distribution constructors over a?
+data Distribution a where
+  Normal :: Value Double -> Value Double -> Distribution Double
+  Beta :: Value Double -> Value Double -> Distribution Double
 
-data Node = Initialized Distribution | Realized Double
+deriving instance Show (Distribution a)
+deriving instance Eq (Distribution a)
 
-newtype Graph = Graph { getGraph :: [Node] }
+pdf :: MonadDistribution m => Distribution a -> a -> DelayedSamplingT m (Log Double)
+pdf (Normal mean stdDev) a = do
+  meanValue <- sample mean
+  stdDevValue <- sample stdDev
+  return $ normalPdf meanValue stdDevValue a
+pdf (Beta _alpha _beta) _ = error "Not implemented beta distribution pdf yet"
+
+data Node a = Initialized (Distribution a) | Realized a
+  deriving (Show, Eq)
+
+data SomeNode = forall a . (Eq a, Show a, Typeable a) => SomeNode { getSomeNode :: Node a }
+
+deriving instance Show SomeNode
+
+instance Eq SomeNode where
+  SomeNode node1 == SomeNode node2 = fromMaybe False $ (node1 ==) <$> cast node2
+
+castNode :: Typeable a => SomeNode -> Maybe (Node a)
+castNode (SomeNode node) = cast node
+
+newtype Graph = Graph { getGraph :: [SomeNode] }
+  deriving (Show, Eq)
 
 empty :: Graph
 empty = Graph []
 
+-- FIXME should be variable, not Int
 data Error
   = IndexOutOfBounds Int
-  deriving Show
+  | TypesInconsistent Int
+  | AlreadyRealized Int
+  -- | Only exists for MonadFail
+  | Fail String
+  deriving (Eq, Show)
 
 newtype DelayedSamplingT m a = DelayedSamplingT { getDelayedSamplingT :: ExceptT Error (StateT Graph m) a }
-  deriving (Functor, Applicative, Monad)
+  deriving (Functor, Applicative, Monad, MonadIO)
 
 instance MonadTrans DelayedSamplingT where
   lift = DelayedSamplingT . lift . lift
+
+instance Monad m => MonadFail (DelayedSamplingT m) where
+
+  fail = throw . Fail
 
 throw :: Monad m => Error -> DelayedSamplingT m a
 throw = DelayedSamplingT . throwE
@@ -74,10 +111,11 @@ throw = DelayedSamplingT . throwE
 tryElse :: Monad m => Error -> Maybe a -> DelayedSamplingT m a
 tryElse e = maybe (throw e) return
 
-lookupVar :: Monad m => Variable -> DelayedSamplingT m Node
+lookupVar :: (Monad m, Typeable a) => Variable a -> DelayedSamplingT m (Node a)
 lookupVar (Variable i) = do
   graph <- DelayedSamplingT $ lift get
-  tryElse (IndexOutOfBounds i) $ lookupListSafe i $ getGraph graph
+  someNode <- tryElse (IndexOutOfBounds i) $ lookupListSafe i $ getGraph graph
+  tryElse (TypesInconsistent i) $ castNode someNode
 
 -- FIXME all these are probably mean that I should use sequences for variables.
 -- FIXME and named variables as well
@@ -90,11 +128,11 @@ replaceListSafe i a as = case splitAt i as of
   (prefix, _ : suffix) -> Just $ prefix ++ (a : suffix)
 
 -- FIXME name sample?
-interpret :: MonadDistribution m => Node -> DelayedSamplingT m Double
+interpret :: MonadDistribution m => Node a -> DelayedSamplingT m a
 interpret (Realized value) = return value
 interpret (Initialized distribution) = interpretDistribution distribution
 
-interpretDistribution :: MonadDistribution m => Distribution -> DelayedSamplingT m Double
+interpretDistribution :: MonadDistribution m => Distribution a -> DelayedSamplingT m a
 interpretDistribution (Normal mean stdDev) = do
   meanValue <- sample mean
   stdDevValue <- sample stdDev
@@ -104,33 +142,93 @@ interpretDistribution (Beta a b) = do
   bValue <- sample b
   lift $ beta aValue bValue
 
-realize :: MonadDistribution m => Variable -> DelayedSamplingT m Double
-realize var@(Variable i) = do
+-- unsafe: doesn't check whether already realized
+realizeAs :: (Typeable a, Monad m, Show a, Eq a) => a -> Variable a -> DelayedSamplingT m ()
+realizeAs a (Variable i) = do
+  graph <- DelayedSamplingT $ lift get
+  nodes <- tryElse (IndexOutOfBounds i) $ replaceListSafe i (SomeNode $ Realized a) (getGraph graph)
+  DelayedSamplingT $ lift $ put $ Graph nodes
+
+realize :: (MonadDistribution m, Typeable a, Show a, Eq a) => Variable a -> DelayedSamplingT m a
+realize var = do
   node <- lookupVar var
   result <- interpret node
-  graph <- DelayedSamplingT $ lift get
-  nodes <- tryElse (IndexOutOfBounds i) $ replaceListSafe i (Realized result) (getGraph graph)
-  DelayedSamplingT $ lift $ put $ Graph nodes
+  realizeAs result var
   return result
 
 -- FIXME name sample?
-sample :: MonadDistribution m => Value a -> DelayedSamplingT m a
+sample :: (MonadDistribution m, Typeable a, Show a, Eq a) => Value a -> DelayedSamplingT m a
 sample (Const a) = return a
 sample (Var var) = realize var
 sample (VarPlus var a) = (a +) <$> realize var
-sample (Fmap f value) = f <$> sample value
-sample (Ap f value) = sample f <*> sample value
+sample (Plus val1 val2) = (+) <$> sample val1 <*> sample val2
+sample (Multiply val1 val2) = (*) <$> sample val1 <*> sample val2
+sample (Abs val) = abs <$> sample val
+sample (Signum val) = signum <$> sample val
+sample (Negate val) = negate <$> sample val
+-- sample (Fmap f value) = f <$> sample value
+-- sample (Ap f value) = sample f <*> sample value
 
-runDelayedSamplingT :: Functor m => DelayedSamplingT m a -> m (Either Error a)
-runDelayedSamplingT = fmap fst . flip runStateT empty . runExceptT . getDelayedSamplingT
+runDelayedSamplingT :: Functor m => DelayedSamplingT m a -> m (Either Error a, Graph)
+runDelayedSamplingT = flip runStateT empty . runExceptT . getDelayedSamplingT
 
-newVar :: Monad m => Distribution -> DelayedSamplingT m Variable
+evalDelayedSamplingT :: Functor m => DelayedSamplingT m a -> m (Either Error a)
+evalDelayedSamplingT = fmap fst . runDelayedSamplingT
+
+newVar :: (Monad m, Typeable a, Show a, Eq a) => Distribution a -> DelayedSamplingT m (Variable a)
 newVar distribution = DelayedSamplingT $ lift $ do
   Graph distributions <- get
-  put $ Graph $ Initialized distribution : distributions
+  -- FIXME this is inefficient
+  put $ Graph $ distributions ++ [SomeNode (Initialized distribution)]
   return $ Variable $ length distributions
 
-normalDS :: Monad m => Value Double -> Value Double -> DelayedSamplingT m (Value Double)
-normalDS mean stdDev = fmap Var $ newVar $ Normal mean stdDev
+normalDS :: Monad m => Value Double -> Value Double -> DelayedSamplingT m (Variable Double)
+normalDS mean stdDev = newVar $ Normal mean stdDev
 
--- observe :: Variable ->
+-- FIXME I'd like to observe on Value a, but I don't know how to do that with var1 + var2
+-- FIXME read the paper again and try to abstract the MARGINALIZE step better
+observe :: (MonadMeasure m, Typeable a, Show a, Eq a) => Variable a -> a -> DelayedSamplingT m ()
+observe variable@(Variable i) a = do
+  node <- lookupVar variable
+  -- FIXME this doesn't yet work if I have realized nodes, those play the same role as Const!
+  case node of
+    -- FIXME this implements the normal distribution as a single-parameter distribution. But there is an exp. family for the double parameter as well!
+    Initialized (Normal (Var varMean) stdDev) -> do
+      -- FIXME this sampling before or after the lookup? in principle they could intertwine, or not? Or is that a topology forbidden by the paper algo?
+      stdDevValue <- sample stdDev
+      meanNode <- lookupVar varMean
+      case meanNode of
+        -- FIXME what does the original algorithm do when these are random as well? sample them first? or send further messages down?
+        Initialized (Normal (Const priorMean) (Const priorStdDev)) -> do
+          let precision = 1 / sqrt (1 / square stdDevValue + 1 / square priorStdDev)
+              newMean = (a / square stdDevValue + priorMean / square priorStdDev) / precision
+          reinitialize varMean $ Normal (Const newMean) (Const $ 1 / sqrt precision)
+        Initialized _prior -> do
+          _mean <- sample $ Var varMean
+          -- FIXME dirty hack because the recursion over the graph is not yet optimal
+          observe variable a
+        Realized mean -> do
+          -- FIXME dirty hack because I treat Const and Realized differently
+          -- FIXME does this handle stdDev correctly?
+          reinitialize variable $ Normal (Const mean) stdDev
+          -- FIXME dirty hack because the recursion over the graph is not yet optimal
+          observe variable a
+    Initialized distribution -> do
+      realizeAs a variable
+      p <- pdf distribution a
+      lift $ score p
+    Realized _ -> throw (AlreadyRealized i)
+
+-- FIXME this is just a step of marginalizing and might have a better name in the paper
+-- FIXME unsafe, doesn't check whether already realized
+reinitialize :: (Monad m, Typeable a, Show a, Eq a) => Variable a -> Distribution a -> DelayedSamplingT m ()
+reinitialize (Variable i) distribution = do
+  graph <- DelayedSamplingT $ lift get
+  nodes <- tryElse (IndexOutOfBounds i) $ replaceListSafe i (SomeNode $ Initialized distribution) (getGraph graph)
+  DelayedSamplingT $ lift $ put $ Graph nodes
+
+debugGraph :: Monad m => DelayedSamplingT m Graph
+debugGraph = DelayedSamplingT $ lift get
+
+debugGraphIO :: MonadIO m => DelayedSamplingT m ()
+debugGraphIO = DelayedSamplingT $ lift get >>= (liftIO . print)
--- test/DelayedSampling.hs
+++ test/DelayedSampling.hs
@@ -5,25 +5,89 @@ import Test.Hspec
 import Control.Monad.Bayes.DelayedSampling
 import Control.Monad.Bayes.Sampler.Strict
 import Control.Monad.Bayes.Class
+import Control.Monad.Bayes.Weighted (runWeighted)
 
 test :: SpecWith ()
 test = describe "DelayedSampling" $ do
+  describe "sampling" $ do
+    it "samples the same value once realized" $ do
+      Right (val1, val2) <- sampleIO $ evalDelayedSamplingT $ do
+        a <- Var <$> normalDS (Const 0) (Const 1)
+        (,) <$> sample a <*> sample a
+      val1 `shouldBe` val2
 
-  it "samples the same value once realized" $ do
-    Right (val1, val2) <- sampleIO $ runDelayedSamplingT $ do
-      a <- normalDS (Const 0) (Const 1)
-      (,) <$> sample a <*> sample a
-    val1 `shouldBe` val2
+    it "samples the same value as a direct draw" $ do
+      Right val1 <- sampleIOfixed $ evalDelayedSamplingT $ do
+        a <- Var <$> normalDS (Const 0) (Const 1)
+        sample a
+      val2 <- sampleIOfixed $ normal 0 1
+      val1 `shouldBe` val2
 
-  it "samples the same value as a direct draw" $ do
-    Right val1 <- sampleIOfixed $ runDelayedSamplingT $ do
-      a <- normalDS (Const 0) (Const 1)
-      sample a
-    val2 <- sampleIOfixed $ normal 0 1
-    val1 `shouldBe` val2
+    it "realizes variables even when applying calculation" $ do
+      Right (val1, val2) <- sampleIO $ evalDelayedSamplingT $ do
+        a <- Var <$> normalDS (Const 0) (Const 1)
+        (,) <$> sample a <*> sample (a + 3)
+      (val1 + 3) `shouldBe` val2
+      Right (a, b, abSum) <- sampleIO $ evalDelayedSamplingT $ do
+        a <- Var <$> normalDS (Const 0) (Const 1)
+        b <- Var <$> normalDS (Const 0) (Const 1)
+        (,,) <$> sample a <*> sample b <*> sample (a + b)
+      (a + b) `shouldBe` abSum
 
-  it "realizes variables even when applying calculation" $ do
-    Right (val1, val2) <- sampleIO $ runDelayedSamplingT $ do
-      a <- normalDS (Const 0) (Const 1)
-      (,) <$> sample a <*> sample (a + 3)
-    (val1 + 3) `shouldBe` val2
+    it "samples independent variables like in direct sampling" $ do
+      Right val1 <- sampleIOfixed $ evalDelayedSamplingT $ do
+        a <- normalDS (Const 0) (Const 1)
+        b <- normalDS (Const 10) (Const 1)
+        (,) <$> sample (Var a) <*> sample (Var b)
+      val2 <- sampleIOfixed $ do
+        a <- normal 0 1
+        b <- normal 10 1
+        return (a, b)
+      val1 `shouldBe` val2
+
+    it "samples variables in a hierarchical model like in direct sampling" $ do
+      Right val1 <- sampleIOfixed $ evalDelayedSamplingT $ do
+        a <- normalDS (Const 0) (Const 1)
+        b <- normalDS (Var a) (Const 1)
+        (,) <$> sample (Var a) <*> sample (Var b)
+      val2 <- sampleIOfixed $ do
+        a <- normal 0 1
+        b <- normal a 1
+        return (a, b)
+      val1 `shouldBe` val2
+
+  describe "observing" $ do
+    it "adds a factor of the PDF when observing" $ do
+      (Right (), p) <- sampleIO $ runWeighted $ evalDelayedSamplingT $ do
+        a <- normalDS (Const 0) (Const 1)
+        observe a 1
+      p `shouldBe` normalPdf 0 1 1
+
+    it "samples the observed value again" $ do
+      (Right x, _) <- sampleIO $ runWeighted $ evalDelayedSamplingT $ do
+        a <- normalDS (Const 0) (Const 1)
+        observe a 1
+        sample $ Var a
+      x `shouldBe` 1
+
+    it "throws an error when observing the same variable twice" $ do
+      (Left e, _) <- sampleIO $ runWeighted $ evalDelayedSamplingT $ do
+        a <- normalDS (Const 0) (Const 1)
+        observe a 1
+        observe a 2
+      e `shouldBe` AlreadyRealized 0
+
+    it "can observe variables in a hierarchical model" $ do
+      ((Right a, graph), p) <- sampleIO $ runWeighted $ runDelayedSamplingT $ do
+        debugGraphIO
+        a <- normalDS (Const 0) (Const 2)
+        debugGraphIO
+        b <- normalDS (Var a) (Const 1)
+        debugGraphIO
+        observe b 1
+        debugGraphIO
+        sample $ Var a
+      print graph
+      print a
+      print p
+      p `shouldBe` normalPdf (4/5) (sqrt (5/4)) a
