98de26cc55328e0c82d07ba562adb7562507db03
Manuel Bärenz <m.baerenz@sonnen.de>
2023-02-08 16:26:38 +0100

    Fixture test SSM model for SMC, RMSMC, PMMH and SMC2

--- test/Spec.hs
+++ test/Spec.hs
@@ -13,6 +13,7 @@ import TestIntegrator qualified
 import TestPipes (hmms)
 import TestPipes qualified
 import TestPopulation qualified
+import TestSSMFixtures qualified
 import TestSampler qualified
 import TestSequential qualified
 import TestStormerVerlet qualified
@@ -167,3 +168,4 @@ main = hspec do
       passed7 `shouldBe` True
 
   TestBenchmarks.test
+  TestSSMFixtures.test
